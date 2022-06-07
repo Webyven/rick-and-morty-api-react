a912c05ea438472e2609293c56607c5cd7e91ac8
Weby <nicoyurec@gmail.com>
2022-06-06 21:05:08 -0300

    Style changes

---
+++ precache-manifest.0395f316a548c5fc3a3b6ca0aa377659.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cd56ed8661894b9d0b7",
+    "url": "/static/css/main.433fbe2a.chunk.css"
+  },
+  {
+    "revision": "2cd56ed8661894b9d0b7",
+    "url": "/static/js/main.2cd56ed8.chunk.js"
+  },
+  {
+    "revision": "8b962f614958ae6dacd7",
+    "url": "/static/js/1.8b962f61.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3625332c56f6e72c17325b067de9104",
+    "url": "/index.html"
+  }
+];
